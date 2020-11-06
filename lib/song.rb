7ea3f3b41b21fac82a3b78f83fe7b5d0d48755ff
Ebony <ebony.furr@yahoo.com>
2020-11-06 17:20:34 -0600

    commit tree c3951dcd3fee3da13a356101679eaceedc71e0e3

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0 
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre) 
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+    
+    def self.count
+        @@count
+    end
+    
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genre_count
+        hash = {}
+        @@genres.each do |genre|
+            hash[genre] ||= 0
+            hash[genre] += 1
+        end
+        hash
+    end
+    def self.artist_count
+        hash = {}
+        @@artists.each do |artists|
+            hash[artists] ||= 0
+            hash[artists] += 1
+        end
+        hash
+    end
+end
